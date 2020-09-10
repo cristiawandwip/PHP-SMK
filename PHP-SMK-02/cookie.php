070e3eb9eb6e4433b16ea8768c0f1c57e47f29aa
cristiawandwip <subaza225@gmail.com>
2020-09-10 08:29:36 +0700

    CookieNew (Setelah Revisi)
    Coding Cookie aslie setelah revisi

--- PHP-SMK-02/cookie.php
+++ PHP-SMK-02/cookie.php
@@ -1,16 +1,12 @@
 <?php
+$cookie_name = "user";
+$cookie_value = "John Doe";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
 
-$cookie_name = 'user';
-$cookie_value = 'joni';
 
-setcookie($cookie_name, $cookie_value);
-
-$cookie_value = 'tejo';
-
-setcookie($cookie_name, $cookie_value);
-
-echo $_COOKIE[$cookie_name];
-
-setcookie("user", "", time() - 3600);
-
-var_dump($_COOKIE);
+if (!isset($_COOKIE[$cookie_name])) {
+    echo "Nama cookie '" . $cookie_name . "' Cookie tidak Jalan!";
+} else {
+    echo " Nama Cookie '" . $cookie_name . "' Cookie Jalan!<br>";
+    echo "Nama User: " . $_COOKIE[$cookie_name];
+}

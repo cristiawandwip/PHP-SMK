946f46cfa84e42679933f4e2757e40f21c311239
cristiawandwip <subaza225@gmail.com>
2020-09-15 18:16:51 +0700

    Versi 1.03
    Minggu ke 10 PHP-SMK

---
+++ Restoran/kategori/update.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once "../function.php";
+
+// echo $id;
+
+$sql = " SELECT * FROM tblkategori WHERE idkategori =$id";
+$result = mysqli_query($koneksi, $sql);
+
+$row = mysqli_fetch_assoc($result);
+
+//echo $row['kategori'];
+
+// $kategori = 'Jelly Bean';
+// $id = 14;
+// $sql = "UPDATE tblkategori SET kategori='$kategori' WHERE idkategori= $id ";
+
+// $result = mysqli_query($koneksi, $sql);
+
+// echo $sql;
+
+?>
+
+
+<form action="" method="post">
+    kategori :
+    <input type="text" name="kategori" value="<?php echo $row['kategori'] ?>">
+    <br>
+    <input type="submit" name="simpan" value="simpan">
+
+</form>
+
+<?php
+
+if (isset($_POST['simpan'])) {
+
+    $kategori = $_POST['kategori'];
+
+    $sql = "UPDATE tblkategori SET kategori='$kategori' WHERE idkategori= $id ";
+
+    $result = mysqli_query($koneksi, $sql);
+
+    header("location:http://localhost/php-smk/Restoran/kategori/select.php");
+}
+
+?>
